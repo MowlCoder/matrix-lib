0c44d9e05fd20b02e2c71f7b24ae524585095e72
Gleb <maikezseller@gmail.com>
2020-12-09 21:57:22 +0300

    Separate random operation in self directory

---
+++ include/random/random.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int MIN_RANDOM_VALUE = 0;
+int MAX_RANDOM_VALUE = 20;
+
+int getRandomNumber(int min=MIN_RANDOM_VALUE, int max=MAX_RANDOM_VALUE);
--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <random>
-#include <ctime>
 #include "./include/matrix/matrix.h"
+#include "./include/random/random.h"
 
 int main() {
-    std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
-    std::uniform_int_distribution die{ 1, 20 };
-
     double **at = new double*[MATRIX_DEFAULT_SIZE];
 
     for (byte i = 0; i < MATRIX_DEFAULT_SIZE; i++) {
         at[i] = new double[MATRIX_DEFAULT_SIZE];
         for (byte j = 0; j < MATRIX_DEFAULT_SIZE; j++) {
-            at[i][j] = die(mersenne);
+            at[i][j] = getRandomNumber();
         }
     }
 
