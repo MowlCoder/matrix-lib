357ea41d92e1cbf0681dba661a9d628c9fea0c7a
Gleb <maikezseller@gmail.com>
2020-12-15 18:14:18 +0300

    fix problem with template error

--- include/random/random.cpp
+++ include/random/random.cpp
@@ -4,6 +4,6 @@
 std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
 
 int getRandomNumber(int min, int max) {
-    std::uniform_int_distribution die{ min, max };
+    std::uniform_int_distribution<> die{ min, max };
     return die(mersenne);
 }
